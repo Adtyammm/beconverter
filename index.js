cc22519dbe362b4fbb97d56f6cde58603b41ccc8
arbaap <arbaadhipamungkas@gmail.com>
2024-04-26 20:21:17 +0700

    add vercel

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ app.get("/", async (req, res, next) => {
   });
 });
 
-app.use("/", convertAudio);
-app.use("/", convertImage);
-app.use("/", mergeAudio);
+app.use("/audio", convertAudio);
+app.use("/img", convertImage);
+app.use("/merge", mergeAudio);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Server is running on port", port));
