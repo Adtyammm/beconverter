a83d456804bf2d733f2d78a0b928c73efb57b6fe
arbaap <arbaadhipamungkas@gmail.com>
2024-04-26 20:06:08 +0700

    test vercel

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ app.use("/", convertAudio);
 app.use("/", convertImage);
 app.use("/", mergeAudio);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Server is running on port", port));
