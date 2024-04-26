ccc975858fdafb29e40db6643fadfcf72e70e2c2
arbaap <arbaadhipamungkas@gmail.com>
2024-04-26 20:25:23 +0700

    asd

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const convertAudio = require("./convertaudio");
+// const convertAudio = require("./convertaudio");
 const convertImage = require("./convertimage");
-const mergeAudio = require("./mergeaudio");
+// const mergeAudio = require("./mergeaudio");
 
 const dbConfig = require("./mongoDB");
 
 app.use(cors());
-
 
 app.use(express.json());
 
@@ -19,9 +18,9 @@ app.get("/", async (req, res, next) => {
   });
 });
 
-app.use("/audio", convertAudio);
+// app.use("/audio", convertAudio);
 app.use("/img", convertImage);
-app.use("/merge", mergeAudio);
+// app.use("/merge", mergeAudio);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Server is running on port", port));
